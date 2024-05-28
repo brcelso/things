913dc0f49f21a79cb84379d6325607a341f7b97a
celso <celso.sjunior@outlook.com>
2024-05-28 17:51:42 +0000

    crash

--- python/month.py
+++ python/month.py
@@ -53,7 +53,7 @@ def count_months(start_date, end_date):
     month_count += 1
 
 # Example usage (assuming today's date is 14-05-2024)
-start_date = "16-11-1957"
+start_date = "06-10-1987"
 end_date = "19-01-2090"
 
 count_months(start_date, end_date)
