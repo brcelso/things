e0077cbec1b5df88865a713df1b2f275b63929e4
celso <celso.sjunior@outlook.com>
2024-05-28 17:17:26 +0000

    ok

--- python/day.py
+++ python/day.py
@@ -52,7 +52,7 @@ def count_days(start_date, end_date):
       print(f"{day_count_str}: {current_date.strftime('%d-%m-%Y')}")
 
 # Example usage (assuming today's date is 14-05-2024)
-start_date = "01-09-2001"
+start_date = "11-09-2001"
 end_date = "19-01-2090"
 
 
