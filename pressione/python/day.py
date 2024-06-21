924ba0407493afe761f669b8a0b974104633fbb3
celso <celso.sjunior@outlook.com>
2024-06-21 14:37:31 +0000

    ok

--- pressione/python/day.py
+++ pressione/python/day.py
@@ -52,7 +52,7 @@ def count_days(start_date, end_date):
       print(f"{day_count_str}: {current_date.strftime('%d-%m-%Y')}")
 
 # Example usage (assuming today's date is 14-05-2024)
-start_date = "07-05-2024"
+start_date = "20-06-2024"
 end_date = "19-01-2055"
 
 
